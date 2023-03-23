f354edb5b48eee147d896a13498fa6d499ae2bb2
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-23 21:44:42 +0530

    making password secure

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,30 +1,36 @@
 const Expense=require('../models/expense');
-
+const bcrypt=require('bcrypt');
 exports.postSignup=(req,res,next)=>{
     console.log(req.body)
-    Expense.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    }).then(result=>{
-        res.status(201).json({newUserDetail:result});
+    bcrypt.hash(req.body.password,10,(err,hash)=>{
+        console.log(err);
+        Expense.create({
+            name:req.body.name,
+            email:req.body.email,
+            password:hash
+        }).then(result=>{
+            res.status(201).json({newUserDetail:result});
+        })
+        .catch(err=>{
+            res.json({newUserDetail:err})
+            console.log(err)})
     })
-    .catch(err=>{
-        res.json({newUserDetail:err})
-        console.log(err)})
 }
 
 exports.postLogin=(req,res,next)=>{
     console.log(req.body.email)
     Expense.findAll({where:{email:req.body.email}}).then(users=>{
         if(users.length>0){
-            if(users[0].password===req.body.password){
-                res.status(200).json({success:true,message:'User logged in successfully'})
-            }else{
-                return res.status(400).json({success:false,message:'Password is incorrect'})
-            }
-        }else{
-            return res.status(404).json({success:false,message:'User does not exist'})
+            bcrypt.compare(req.body.password,users[0].password,(err,result)=>{
+                if(err){
+                    throw new Error('Something went wrong');
+                }
+                if(result===true){
+                    res.status(200).json({success:true,message:'User logged in successfully'})
+                }else{
+                    return res.status(400).json({success:false,message:'Password is incorrect'})
+                }
+            })
         }
     })
     .catch(err=>{
