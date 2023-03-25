8b480787f9471e84b0e28bee83ec4ae42ca4f5b2
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-25 19:08:39 +0530

    query optimizations

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,24 +1,18 @@
 const ExpenseItems = require("../models/expense");
-const User = require("../models/user")
+const User = require("../models/user");
+const sequelize = require("../util/database");
 
 exports.getLeaderboard=async(req,res,next)=>{
-   const users=await User.findAll();
-   const expenses=await ExpenseItems.findAll();
-   const combined={};
-   expenses.forEach(expense=>{
-    if(combined[expense.userId]){
-        combined[expense.userId]+=expense.expense
-    }else{
-        combined[expense.userId]=expense.expense
-    }
-   })
-   const entireUserDetails=[];
-   users.forEach(user=>{
-      entireUserDetails.push({name:user.name,total:combined[user.id]})
-   })
-   console.log(combined);
-   entireUserDetails.sort((a,b)=>{
-    return b.total-a.total
-   })
-   res.status(200).json(entireUserDetails);
+   const users=await User.findAll({
+    attributes:['id','name',[sequelize.fn('sum',sequelize.col('expense-items.expense')),'total']],
+    include:[
+        {
+            model:ExpenseItems,
+            attributes:[]
+        }
+    ],
+    group:['user.id'],
+    order:[['total','DESC']]
+});
+   res.status(200).json(users);
 }
