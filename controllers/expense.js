9a3852fc10966bb679e692fa530348981f519a5d
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-25 20:45:20 +0530

    using transaction and correcting deleting bugs

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,5 +1,6 @@
 const ExpenseItems=require('../models/expense');
 const User = require('../models/user');
+const sequelize = require('../util/database');
 
 exports.getExpenses=(req,res,next)=>{
     ExpenseItems.findAll({where:{userId:req.user.id}}).then(result=>{
@@ -9,35 +10,54 @@ exports.getExpenses=(req,res,next)=>{
     .catch(err=>console.log(err))
   }
 
-exports.postExpenses=(req,res,next)=>{
+exports.postExpenses=async(req,res,next)=>{
+    try{
+    const t=await sequelize.transaction()
     console.log(req.body);
-    ExpenseItems.create({
+    const result =await ExpenseItems.create({
         expense:req.body.expense,
         description:req.body.description,
         category:req.body.category,
         userId:req.user.id
-    }).then(result=>{
+    },{transaction:t})
         const totalExpense=Number(req.user.totalExpense)+Number(req.body.expense)
         console.log(totalExpense);
-        User.update({
+        await User.update({
             totalExpense:totalExpense
         },{
             where:{
                 id:req.user.id
-            }
-        }).then(()=>{
+            },
+            transaction:t
+        })
+            await t.commit();
             res.status(201).json({newExpenseDetail:result})
             console.log(result)
-        })
-    })
-    .catch(err=>console.log(err))
+    }
+    catch(err){
+        await t.rollback();
+        res.status(403).json({success:true,message:'Something went wrong'})
+    }
 }
 
-exports.getDelete=(req,res,next)=>{
+exports.getDelete=async (req,res,next)=>{
+    try{
+    const t=await sequelize.transaction()
     const userId=req.params.userId;
-    ExpenseItems.destroy({where:{id:userId,userId:req.user.id}}).then((result)=>{
-       console.log(result)
-       console.log('deleted')
-    })
-    .catch(err=>console.log(err))
+    const results=await ExpenseItems.findAll({where:{id:userId,userId:req.user.id}},{transaction:t})
+        await results[0].destroy()
+            console.log('deleted')
+            const totalExpense=Number(req.user.totalExpense)-Number(results[0].expense)
+            await User.update({
+                totalExpense:totalExpense
+            },{where:{
+                id:req.user.id
+            },
+            transaction:t
+        })
+            await t.commit();
+        }catch(err){
+            await t.rollback();
+            res.status(403).json({success:false,message:'Expense not deleted'})
+        }
 }
